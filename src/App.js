03c33cf0c2584216b9e6203d36dc9eef1ea50d95
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-30 19:31:46 +0400

    :zap: Props To connect search in app.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import Search from "./components/search/search";
 
 export default function App() {
+  const [input, setInput] = useState("");
   return (
     <>
-      <p>Hello World</p>
+      <Search value={input} onChange={(e) => setInput(e.target.value)} />
     </>
   );
 }
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Search() {
+export default function Search({ value, onChange }) {
   return (
     <>
       <form>
-        <input type="text" />
+        <input type="text" value={value} onChange={onChange} />
       </form>
     </>
   );
